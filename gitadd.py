583a626f432d354bbb7901789a485db9191fb103
Yaron Nechushtan <yaron.nechushtan@gmail.com>
2020-03-29 23:41:18 +0300

    blatant

---
+++ gitadd.py
@@ -0,0 +1,10 @@
+
+import os
+from random_word import RandomWords
+r = RandomWords()
+
+os.system("git add *")
+os.system("git commit -m " + r.get_random_word())
+os.system("git push")
+os.system("git push heroku master")
+
