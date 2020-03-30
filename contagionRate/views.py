13a5a1e70fd6785f0f37961574006ae8f021e1c2
Yaron Nechushtan <yaron.nechushtan@gmail.com>
2020-03-30 17:47:09 +0300

    leftish

--- contagionRate/views.py
+++ contagionRate/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests, re
+import requests, re, math
 import matplotlib.pyplot as plt 
 import numpy as np; np.random.seed(1)
 import matplotlib.pyplot as plt, mpld3
