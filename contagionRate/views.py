f184860d3a29072c785ac1a69dc80d764b26a38b
Yaron Nechushtan <yaron.nechushtan@gmail.com>
2020-06-20 14:22:03 +0300

    rosiness

--- contagionRate/views.py
+++ contagionRate/views.py
@@ -38,6 +38,7 @@ def index(request):
   """
 
   # Set headers
+  
   headers = requests.utils.default_headers()
   headers.update({ 'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'})
 
