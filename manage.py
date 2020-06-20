107c5c35ccf06d33ec359c7d0621e7d96e090a92
Yaron Nechushtan <yaron.nechushtan@gmail.com>
2020-06-20 14:34:05 +0300

    refutes

--- manage.py
+++ manage.py
@@ -3,6 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
+    print("HERE") 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gettingstarted.settings")
 
     from django.core.management import execute_from_command_line
